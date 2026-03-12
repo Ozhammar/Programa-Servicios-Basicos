c5c390a24d55e118e355a9ffd593cf326b41826f
Lucas <Lucas.povolo1@gmail.com>
2026-03-12 11:55:49 -0300

    --IMPLEMENTACION DE ENUM PARA DEFINIR TIPOS DE SERVICIOS
    --IMPLEMENTACION DE UN DICCIONARIO DE TIPOS DE SERVICIO COMO CLAVE Y OBJETO DE GASTO Y CODIGO DE CATALOGO COMO VALORES
    
    --REORDENAMIENTO DE LA FUNCION DE GENERACION DE FACTURA DENTRO DE UNA ESTRUCTURA TRY-CATCH PARA QUE POR MAS QUE NO SE ENCUENTREN LOS DATOS EN EL DICCIONARIO, SE GENERE LA FACTURA
    
    --IMPLEMENTACION DE UNA FUNCION PARA BUSCAR AUTOMATICAMENTE EL OBJETO DE GASTO Y EL CODIGO DE CATALOGO DE CADA TIPO DE FACTURA, INDISTINTAMENTE DE SI ES UN SERVICIO DE INTERIOR O NO.

--- Control de Facturas/Processors/ProcesadorCargaManual.cs
+++ Control de Facturas/Processors/ProcesadorCargaManual.cs
@@ -2,47 +2,93 @@
 {
     internal class ProcesadorCargaManual
     {
-        private enum TipoServicio 
+        private enum TipoServicio
         {
             ELECTRICIDAD, AGUA, GAS
         }
+        private readonly Dictionary<TipoServicio, DatosCatalogo> CatalogoServicios;
+        public ProcesadorCargaManual()
+        {
+            CatalogoServicios = new Dictionary<TipoServicio, DatosCatalogo>()
+            {
+                {
+                    TipoServicio.ELECTRICIDAD, new DatosCatalogo
+                    {
+                        ObjetoGasto = "3.1.1.0", CodigoCatalogo ="3.1.1-2390-1"
+                    }
+                },
+                {
+                    TipoServicio.AGUA, new DatosCatalogo
+                    {
+                        ObjetoGasto = "3.1.2.0", CodigoCatalogo ="3.1.2-2391-1"
+                    }
+                },
+                {
+                    TipoServicio.GAS, new DatosCatalogo
+                    {
+                        ObjetoGasto = "3.1.3.0", CodigoCatalogo ="3.1.3-2392-1"
+                    }
+                },
+            };
+        }
 
-        Dictionary<TipoServicio, DatosCatalogo>
-
-        /*
-            factura.ObjetoGasto = "3.1.2.0"; // Objeto de gasto fijo para AGUA
-            factura.CodigoCatalogo = "3.1.2-2391-1"; // Código de catálogo fijo para AGUA
-            factura.ObjetoGasto = "3.1.3.0"; // Objeto de gasto fijo para GAS
-            factura.CodigoCatalogo = "3.1.3-2392-1"; // Código de catálogo fijo para GAS
-            factura.ObjetoGasto = "3.1.1.0"; // Objeto de gasto fijo para Edesur
-            factura.CodigoCatalogo = "3.1.1-2390-1"; // Código de catálogo fijo para Edesur
-
-         
-         */
         public Factura ProcesarFactura(DatosFactura datos)
         {
             Factura factura = new Factura();
+            try
+            {
+                factura.Empresa = datos.Denominacion;
+                factura.NumeroCliente = datos.Cliente;
+                factura.TipoFactura = datos.TipoFactura;
+                factura.PuntoVenta = datos.PuntoVenta;
+                factura.NumeroFactura = datos.NumeroFactura;
+                factura.FechaEmision = datos.FechaEmision;
+                factura.FechaVencimiento = datos.FechaVencimientoFC;
+                factura.Periodo = datos.Periodo;
+                factura.ImportePrimerVencimiento = datos.Importe;
+                factura.CUIT = datos.CUIT;
+                factura.TipoCodigoAutorizacion = datos.TipoCodAut;
+                factura.CodigoAutorizacion = datos.CodigoAut;
+                factura.VencimientoCodigoAutorizacion = datos.VencimientoCodAut;
+                //factura.Archivo =
+                factura.TipoServicio = datos.TipoServicio;
+                //factura.Tarifa
 
-            factura.Empresa = datos.Denominacion;
-            factura.NumeroCliente = datos.Cliente;
-            factura.TipoFactura = datos.TipoFactura;
-            factura.PuntoVenta = datos.PuntoVenta;
-            factura.NumeroFactura = datos.NumeroFactura;
-            factura.FechaEmision = datos.FechaEmision;
-            factura.FechaVencimiento = datos.FechaVencimientoFC;
-            factura.Periodo = datos.Periodo;
-            factura.ImportePrimerVencimiento = datos.Importe;
-            factura.CUIT = datos.CUIT;
-            //factura.ObjetoGasto =
-            //factura.CodigoCatalogo =
-            factura.TipoCodigoAutorizacion = datos.TipoCodAut;
-            factura.CodigoAutorizacion = datos.CodigoAut;
-            factura.VencimientoCodigoAutorizacion = datos.VencimientoCodAut;
-            //factura.Archivo =
-            factura.TipoServicio = datos.TipoServicio;
-            //factura.Tarifa
+                var datosCatalogo = BuscarDatosCatalogo(datos);
+                factura.ObjetoGasto = datosCatalogo.ObjetoGasto;
+                factura.CodigoCatalogo = datosCatalogo.CodigoCatalogo;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontró el servicio en el catálogo.");
+            }
 
             return factura;
+        }
+
+        private DatosCatalogo BuscarDatosCatalogo(DatosFactura datos)
+        {
+            try
+            {
+                string datosTipoServicio = datos.TipoServicio;
+                if (datosTipoServicio.Contains("INTERIOR"))
+                {
+                    datosTipoServicio = datosTipoServicio.Replace(" INTERIOR", "").Trim();
+                }
+                if (Enum.TryParse<TipoServicio>(datosTipoServicio, out var tipoServicio))
+                {
+                   
+                    if (CatalogoServicios.TryGetValue(tipoServicio, out var datosCatalogo))
+                    {
+                        return datosCatalogo;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontró el servicio en el catálogo.");
+            }
+            return null;
         }
 
 
